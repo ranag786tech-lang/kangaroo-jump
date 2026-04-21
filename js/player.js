4c196779fe52771dfc5b3590f53460f40a3bcb18
RANA Hamza <253780593+ranag786tech-lang@users.noreply.github.com>
2026-04-21 18:09:15 +0500

    Implement Player class with movement and jump methods

--- js/player.js
+++ js/player.js
@@ -1,1 +1,38 @@
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.w = CONFIG.spriteSize;
+    this.h = CONFIG.spriteSize;
+    this.vx = 0;
+    this.vy = -5;
+    this.sprite = new Image();
+    this.sprite.src = "assets/sprites/kangaroo.png";
+    this.frame = 0;
+  }
 
+  update() {
+    this.vy += CONFIG.gravity;
+    this.x += this.vx;
+    this.y += this.vy;
+
+    if (this.x < 0) this.x = canvas.width;
+    if (this.x > canvas.width) this.x = 0;
+
+    this.frame = (this.frame + 1) % 4; // cycle frames
+  }
+
+  jump() {
+    this.vy = CONFIG.jumpForce;
+    playSound("assets/sounds/jump.wav");
+  }
+
+  draw() {
+    ctx.drawImage(
+      this.sprite,
+      this.frame * CONFIG.spriteSize, 0,
+      CONFIG.spriteSize, CONFIG.spriteSize,
+      this.x, this.y, this.w, this.h
+    );
+  }
+}
